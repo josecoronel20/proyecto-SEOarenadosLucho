ee800201e8bcbafd3494f6d37106037cdaf86723
josecoronel20 <josecoronel20@outlook.com>
2025-12-18 08:48:43 -0300

    add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -237,6 +237,18 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W63ZV9D9');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Meta tags esenciales */}
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -290,6 +302,17 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W63ZV9D9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <GoogleAnalytics />
         <Header />
         <main className="pt-16">{children}</main>
